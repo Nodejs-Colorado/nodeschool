8a382f1ca23a111b64afc78e7486acc005e83f28
Michael Herman <hermanmu@gmail.com>
2014-08-01 17:52:47 -0500

    file server

---
+++ learnyounode/file-server.js
@@ -0,0 +1,20 @@
+var http = require('http');  // require http client
+var fs = require('fs');      // access file system
+
+console.log(process.argv[3])
+
+// create server
+http.createServer(function(req, res){
+  fs.createReadStream(process.argv[3]).pipe(res); // pipe method used to convert command line arg to HTTP response
+}).listen(process.argv[2]) // grab port from first command line arg
+
+
+
+
+
+
+
+
+
+
+
