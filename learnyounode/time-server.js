59a50375da5a7f56ceec85a49f3b429535a5c8df
Michael Herman <hermanmu@gmail.com>
2014-08-01 17:37:56 -0500

    time-server

---
+++ learnyounode/time-server.js
@@ -0,0 +1,24 @@
+var net = require('net')
+
+function padded(timeChunk){
+  console.log(timeChunk)
+  if(timeChunk < 10){
+    return '0'+timeChunk;
+  } else {
+    return ''+timeChunk;
+  }
+}
+
+// create server
+var server = net.createServer(function(socket) {
+  date_time = new Date();
+  // console.log(date_time)
+  var year = padded(date_time.getFullYear());
+  var month = padded(date_time.getMonth()+1);
+  var day = padded(date_time.getDate());
+  var hour = padded(date_time.getHours());
+  var minute = padded(date_time.getMinutes());
+  socket.write(year + "-" + month + "-" + day + " " + hour + ":" + minute);
+  socket.write('\n');
+  socket.end();
+}).listen(process.argv[2]); // listen to server on specific port
