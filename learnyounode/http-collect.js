6945a83c7249303ead475f77c1655fb33f657cb6
Michael Herman <hermanmu@gmail.com>
2014-08-01 11:20:56 -0500

    http collect stream

--- learnyounode/http-collect.js
+++ learnyounode/http-collect.js
@@ -2,7 +2,8 @@
 var http = require('http')
 // grab the url
 var url = process.argv[2]
-
+// variable for collecting all data
+var all_data = ''
 
 http.get(url, function (response) {
 
@@ -10,7 +11,12 @@ http.get(url, function (response) {
 
   // '.on' binds the callback (response) to an event
   response.on('data', function (chunk) {
-    console.log(chunk.toString());
+    all_data += chunk.toString(); // concatenate all chunks of data
+  });
+  // verifies everything all data has been sent from the server
+  response.on('end', function() {
+    console.log(all_data.length)
+    console.log(all_data)
   });
   response.on('error', function (chunk) {
     console.log(chunk.toString());
