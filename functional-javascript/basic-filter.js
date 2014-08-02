2d1cc0493ce1c0733e026134b9447a1f168d4c93
Michael Herman <hermanmu@gmail.com>
2014-08-02 10:16:46 -0500

    basic filter

--- functional-javascript/basic-filter.js
+++ functional-javascript/basic-filter.js
@@ -1,24 +1,7 @@
-// loop
-module.exports = function repeat_loop(operation, num) {
-  if (num > 0) {
-    for (i=0; i<num; i++) {
-      operation()
-    };
-  };
-};
-
-
-// // recursion
-// module.exports = function repeat_recursion(operation, num) {
-//   if (num > 0) {
-//     operation();
-//     return repeat(operation, num - 1);
-//   };
-// };
-
-
-module.exports = function double(numbers) {
-  return numbers.map(function (number) {
-    return number * 2;
+module.exports = function getShortMessages(messages) {
+  return messages.filter(function (item) {
+    return item.message.length < 50;
+  }).map(function (item) {
+    return item.message;
   });
 }
